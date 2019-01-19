b0804dde21436f45bea65f2c132b53621de87d3f
Nighat Ansari <na295@cornell.edu>
2019-01-19 15:49:45 -0500

    fixing

--- www/scripts/slideShow.js
+++ www/scripts/slideShow.js
@@ -1,6 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function slides() {
 
   var images = [
+    "images/amex.png",
     "images/amex.png",
     "images/thermo.png",
     "images/comp.png",
@@ -9,17 +10,34 @@ $(document).ready(function() {
     "images/brh.png"
   ];
 
+  var containers = [
+    "#work-intro",
+    "amex-container",
+    "thumo-container",
+    "comp-container",
+    "rock-container",
+    "sub-container",
+    "brh-container"
+  ]
+
 
   var currentIndex = 0;
+  var currentContainer = 0;
 
   $("#slideshowNext").click(function() {
     currentIndex += 1;
+    currentContainer += 1;
+    //ADD A RANGE FOR LOOP HERE THAT HIDES ALL THE OTHER DIVS NOT AT THIS PARTICULAR INDEX.
+
 
     if (currentIndex >= images.length) {
       currentIndex = 0;
+      currentContainer = 0;
     }
 
     $("#slideshowimage").attr("src", images[currentIndex]);
+    $(containers[currentContainer]).removeClass("hidden");
 
   });
+
 });
