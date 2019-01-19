13a08cb8ee8813c440100c4b1385df6b8d897f5f
Nighat Ansari <na295@cornell.edu>
2019-01-19 15:59:12 -0500

    coding div boxes to show up on slides.

---
+++ www/scripts/clicks.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $("#amex-learn").click(function () {
+    $(".amex-para2").removeClass("hidden");
+
+  });
+
+  $("#thumo-learn").click(function () {
+    $(".thumo-para2").removeClass("hidden");
+  });
+
+  $("#comp-learn").click(function () {
+    $(".comp-para2").removeClass("hidden");
+  });
+
+  $("#rock-learn").click(function () {
+    $(".rock-para2").removeClass("hidden");
+  });
+
+  $("#sub-learn").click(function () {
+    $(".sub-para2").removeClass("hidden");
+  });
+
+
+
+  });
